c8d989937f0c6ab197824a5b177d0bd2f166df09
Oskar <Oskar>
2023-06-22 15:17:57 +0200

    Protecting the page from using hard endpoints

--- page_with_logging/main.go
+++ page_with_logging/main.go
@@ -10,6 +10,9 @@ import(
 	"strings"
 	"regexp"
 )
+var logged bool = false
+var finished bool = false
+
 func errorCheck(err error){
 	if err != nil{
 		log.Fatal(err)
@@ -65,6 +68,8 @@ func handleCreate(writer http.ResponseWriter,
 				err = file.Close()
 				errorCheck(err)
 
+				finished = true
+
 				http.Redirect(writer, request, "/createAccount/successRegistered", http.StatusFound)
 		}else if (passwordConfirm != password || match == true) && nick != password{
 			http.Redirect(writer, request, "/createAccount/failedRegistered", http.StatusFound)
@@ -88,6 +93,7 @@ func handleLogin(writer http.ResponseWriter,
 			item := strings.Split(v, " ")
 			if nick==item[0] && password==item[1]{
 				fmt.Printf("%s with password %s logged in\n",nick,password)
+				logged = true
 				http.Redirect(writer, request, "/login/successLogin", http.StatusFound)
 			}
 		}
@@ -112,20 +118,54 @@ func handleFailed(writer http.ResponseWriter,
 		err = tmpl.Execute(writer, nil)
 	}
 
-func handleSuccess(writer http.ResponseWriter,
+func handleErrorWithLogging(writer http.ResponseWriter,
 	request *http.Request){
-		tmpl, err := template.ParseFiles("success.html")
+		tmpl, err := template.ParseFiles("notlogin.html")
 		errorCheck(err)
 		err = tmpl.Execute(writer, nil)
+	}
+
+func handleNotFinished(writer http.ResponseWriter,
+	request *http.Request){
+		tmpl, err := template.ParseFiles("notFinished.html")
+		errorCheck(err)
+		err = tmpl.Execute(writer, nil)
+	}
+
+func handleSuccess(writer http.ResponseWriter,
+	request *http.Request){
+		if logged == false {
+			http.Redirect(writer, request, "/login/notLoggedIn", http.StatusFound)
+		}else if logged == true {
+			tmpl, err := template.ParseFiles("success.html")
+			errorCheck(err)
+			err = tmpl.Execute(writer, nil)
+		}
 	}
 
 func handleSuccessReg(writer http.ResponseWriter,
 	request *http.Request){
-		tmpl, err := template.ParseFiles("successRegistered.html")
-		errorCheck(err)
-		err = tmpl.Execute(writer, nil)
+		if finished == false {
+			http.Redirect(writer, request, "/createAccount/notFinishedForm",http.StatusFound)
+		}else if finished == true {
+			tmpl, err := template.ParseFiles("successRegistered.html")
+			errorCheck(err)
+			err = tmpl.Execute(writer, nil)
+		}
 	}
 
+func handleLogout(writer http.ResponseWriter,
+	request *http.Request){
+		logged = false
+		fmt.Printf("Logging out...")
+		http.Redirect(writer, request, "/",http.StatusFound)
+	}
+
+func handleReset(writer http.ResponseWriter,
+	request *http.Request){
+		finished = false
+		http.Redirect(writer, request, "/",http.StatusFound)
+	}
 func main(){
 	http.HandleFunc("/", handleMain)
 	http.HandleFunc("/login", handleLogin)
@@ -136,6 +176,11 @@ func main(){
 
 	http.HandleFunc("/login/failedLogin",handleFailedLogin)
 	http.HandleFunc("/createAccount/failedRegistered",handleFailed)
+	http.HandleFunc("/login/notLoggedIn",handleErrorWithLogging)
+	http.HandleFunc("/createAccount/notFinishedForm", handleNotFinished)
+
+	http.HandleFunc("/login/logout", handleLogout)
+	http.HandleFunc("/createAccount/successRegistered/reset",handleReset)
 
 	err := http.ListenAndServe("localhost:8080", nil)
 	log.Fatal(err)
